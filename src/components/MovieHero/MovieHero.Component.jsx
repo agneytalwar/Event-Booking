7b2818a2f19ddb81120f6f309315e686c0705f69
agneytalwar <agneytalwar@gmail.com>
2021-12-19 18:06:18 +0530

    changed useContext import

--- src/components/MovieHero/MovieHero.Component.jsx
+++ src/components/MovieHero/MovieHero.Component.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState ,useContext} from "react/cjs/react.development";
+import { useState ,useContext} from "react";
 import MovieInfo from "./MovieInfo.Component";
 
 //context
